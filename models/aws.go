ba2197b79f453c124c6a5a93db39360c56cf204c
hungnguyendang <nguyendanghung2008@gmail.com>
2015-09-20 12:15:52 +0700

    update

--- models/aws.go
+++ models/aws.go
@@ -94,10 +94,10 @@ func GetDuration(Duration uint32) string {
 func ExtractMp4Meta(Dir string) (uint32, uint32, string, int, int) {
     m, err := mp4.Open(Dir)
 	if err != nil {
-	  log.Printf(".........ExtractMp4Meta Error:%s", err)
+	  fmt.Println(".........ExtractMp4Meta Error:%s", err)
 	}
-    log.Printf("video %dx%d", m.W, m.H)
-    log.Printf("duration %d", uint32(m.Dur))
+    fmt.Println("video %dx%d", m.W, m.H)
+    fmt.Println("duration %d", uint32(m.Dur))
     log.Println("duration time:%s", GetDuration(uint32(m.Dur)))    
     return uint32(m.Dur), 1, GetDuration(uint32(m.Dur)), m.W,  m.H
  
@@ -111,35 +111,35 @@ func UploadVideo(Url string) (string, string, uint32, uint32, string) {
 	fmt.Println("Downloading", Url, "to", fileName)
 	file, err := os.Create(fileName)
 	if err != nil {
-		log.Printf("Error while creating", fileName, "-", err)
+		fmt.Println("Error while creating", fileName, "-", err)
 		return "", "", 0, 0,""
 	}
 	defer file.Close()
 
 	response, err := http.Get(Url)
 	if err != nil {
-		log.Printf("Error while downloading", Url, "-", err)
+		fmt.Println("Error while downloading", Url, "-", err)
 		return "", "", 0, 0,""
 	}
 	defer response.Body.Close()
 
 	n, err := io.Copy(file, response.Body)
 	if err != nil {
-		log.Printf("Error while downloading", Url, "-", err)
+		fmt.Println("Error while downloading", Url, "-", err)
 		return "", "", 0, 0,""
 	}
-	log.Printf("n:%s", n)
+	fmt.Println("n:%s", n)
 
 	dir, err := filepath.Abs(fileName)
 	if err != nil {
-		log.Printf("error file Abs:%s\n", err)
+		fmt.Println("error file Abs:%s\n", err)
 	}
-	log.Printf(".......file path:%s\n", dir)
+	fmt.Println(".......file path:%s\n", dir)
     
     duration, timescale, durationstr, width, height := ExtractMp4Meta(dir)
 	
-    log.Printf("duaration:%d, timescale:%d, durationstr:%s, width:%d, height:%d",duration, timescale, durationstr, width, height)
-	log.Printf("Start upload file to S3:%s\n", fileName)
+    fmt.Println("duaration:%d, timescale:%d, durationstr:%s, width:%d, height:%d",duration, timescale, durationstr, width, height)
+	fmt.Println("Start upload file to S3:%s\n", fileName)
 	uploader := s3manager.NewUploader(nil)
 	result, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: &bucket,
@@ -147,9 +147,9 @@ func UploadVideo(Url string) (string, string, uint32, uint32, string) {
 		Body:   file,
 	})
 	if err != nil {
-		log.Printf("Failed to upload", err)
+		fmt.Println("Failed to upload", err)
 	}
-	log.Println("Uploaded....%s", result)
+	fmt.Println("Uploaded....%s", result)
     err = os.Remove(dir)
     if err != nil {
     }
